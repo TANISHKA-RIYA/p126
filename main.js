cf3561622b5cf82e14531aa811d5139d05c3d2aa
TANISHKA-RIYA <106598143+TANISHKA-RIYA@users.noreply.github.com>
2023-09-06 19:57:24 +0530

    p128

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music1 = "";
 music2 = "";
+leftwristX = 0;
+leftwristY = 0;
+rightwristX = 0;
+rightwristY = 0;
 
 function preload(){
     music1 = loadSound("music.mp3");
@@ -10,7 +14,25 @@ function setup(){
     canvas = createCanvas(500,500);
     video.hide();
     canvas.center();
+    posenet = ml5.poseNet(video , modelLoaded);
+    posenet.on("pose" , gotPoses);
 }
 function draw(){
     image(video,0,0,500,500);
+}
+function modelLoaded(){
+    console.log("posenet is initialized");
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+
+       leftwristX = results[0].pose.leftWrist.x;
+       leftwristY = results[0].pose.leftWrist.y;
+       console.log("leftwristx = "+leftwristX + " leftwristy = "+leftwristY);
+
+       rightwristX = results[0].pose.rightWrist.x;
+       rightwristY = results[0].pose.rightWrist.y;
+       console.log("rightwristx = "+rightwristX + " rightwristy = "+rightwristY);
+  }
 }
